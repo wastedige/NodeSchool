68c52203f172cde6d1128ab437c5d1b47f2d9976
Shaahin <shaahin@gmail.com>
2016-04-10 15:21:15 -0500

    FS readFile async

---
+++ LearnYouNode/firstio-async.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var str;
+function readlines(callback) {
+  fs.readFile(process.argv[2], function(err, data){
+    if (err) throw err;
+    str = data.toString().split('\n');
+    callback();
+  });
+}
+
+function countOutput() {
+    console.log(str.length - 1);
+}
+
+readlines(countOutput);
